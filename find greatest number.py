da559b6aa2e79c1383f48f20fbfc333ab9e98530
SYS_03 <jayakumari.sjk27@gmail.com>
2021-09-11 19:24:38 +0530

    basic learning

---
+++ find greatest number.py
@@ -0,0 +1,12 @@
+a=int(input("enter the 1st value"))
+b=int(input("enter the 2st value"))
+c=int(input("enter the 3st value"))
+if a>b:
+    if a>c:
+        print("f A is greater")
+    else:
+        print("f C is greater")
+else:
+    if b>c:
+        print("f b is greater")
+    else:print("f c is greater")
