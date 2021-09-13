1bc25f935e03affeade64a71ae81555f7439a2b8
SYS_03 <jayakumari.sjk27@gmail.com>
2021-09-13 20:48:35 +0530

    basic learning

--- hotelprogram.py
+++ hotelprogram.py
@@ -7,13 +7,15 @@ if option==1:
         print(f"{varities} coming soon")
     elif dishes=="2":
         varities=input("enter the varities type\n-1 sambar--RS30\n2 curd--Rs25")
-        print(f"{varities}coming soon")
+        print(f"{varities} coming soon")
 elif option==2:
+    print("non_veg")
+    dishes = input("enter the dishes type\n1-breakfast\n2-lunch")
     if dishes=="1":
         varities=input("enter the\n1-Idaly+chickencurry--60\n2-parota--20")
         print(f"{varities}coming soon")
     elif dishes=="2":
         varities=input("enter the varities\n 1-briyani RS-100\n 2-NV meals Rs-150")
-        print(f"{varities}coming soon")
+        print(f"{varities} coming soon")
 else:
     print("invalid")
