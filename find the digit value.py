4cc1c215f9d8be499a1ff300b1b3ab1eedbd9a7e
SYS_03 <jayakumari.sjk27@gmail.com>
2021-09-18 20:13:07 +0530

    basic learning

---
+++ find the digit value.py
@@ -0,0 +1,14 @@
+a=int(input("enter the 1st value"))
+b=int(input("enter the 2nd value"))
+if a>10 and a<99:
+    if b>10 and b<99:
+        c=a+b
+        print(c)
+    else:
+        c=a-b
+        print(c)
+else:
+    print("invalide")
+
+
+
